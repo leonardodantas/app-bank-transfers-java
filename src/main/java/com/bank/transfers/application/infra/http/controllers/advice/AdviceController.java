de8eb80630c59b8ff170b4dad113357d0d8b5153
leo <leonardo1317@yahoo.com.br>
2022-09-11 20:16:59 -0300

    Create all advices

--- src/main/java/com/bank/transfers/application/infra/http/controllers/advice/AdviceController.java
+++ src/main/java/com/bank/transfers/application/infra/http/controllers/advice/AdviceController.java
@@ -1,17 +1,17 @@
 package com.bank.transfers.application.infra.http.controllers.advice;
 
-import com.bank.transfers.application.app.exceptions.AlreadyUserException;
-import com.bank.transfers.application.app.exceptions.BankAccountNotFoundException;
+import com.bank.transfers.application.app.exceptions.*;
 import com.bank.transfers.application.infra.http.responses.ErrorResponse;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.security.auth.login.AccountNotFoundException;
+import java.util.Collection;
+import java.util.List;
 
 @ControllerAdvice
 public class AdviceController {
@@ -25,24 +25,45 @@ public class AdviceController {
     @ExceptionHandler(AlreadyUserException.class)
     public ResponseEntity<ErrorResponse> handlerAlreadyUserException(final AlreadyUserException exception) {
         final var response = ErrorResponse.from(exception.getMessage());
-        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(BankAccountNotFoundException.class)
     public ResponseEntity<ErrorResponse> handlerBankAccountNotFoundException(final BankAccountNotFoundException exception) {
         final var response = ErrorResponse.from(exception.getMessage());
-        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(response);
     }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handlerMethodArgumentNotValidException(final MethodArgumentNotValidException exception) {
-        final var fields = exception.getBindingResult().getFieldErrors();
 
-        final var errors = fields.stream()
+    @ExceptionHandler(BankAccountNotActiveException.class)
+    public ResponseEntity<ErrorResponse> handlerBankAccountNotActiveException(final BankAccountNotActiveException exception) {
+        final var response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(LogisticsTransferException.class)
+    public ResponseEntity<ErrorResponse> handlerLogisticsTransferException(final LogisticsTransferException exception) {
+        final var response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(WithoutBalanceException.class)
+    public ResponseEntity<ErrorResponse> handlerWithoutBalanceException(final WithoutBalanceException exception) {
+        final var response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Collection<ErrorResponse>> handlerMethodArgumentNotValidException(final MethodArgumentNotValidException exception) {
+        final var fields = exception.getBindingResult().getFieldErrors();
+        final var errors = getErrorResponses(fields);
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    private List<ErrorResponse> getErrorResponses(final Collection<FieldError> fields) {
+        return fields.stream()
                 .map(field ->
                         ErrorResponse.of(field.getField(), messageSource.getMessage(field, LocaleContextHolder.getLocale()))
                 )
                 .toList();
-
-        return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
     }
 }
