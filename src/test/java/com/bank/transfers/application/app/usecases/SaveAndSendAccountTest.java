87b6f6b5254c2ff274b34aab753a3cba5ed9f15f
leo <leonardo1317@yahoo.com.br>
2022-09-21 08:18:01 -0300

    Adjustment tests

--- src/test/java/com/bank/transfers/application/app/usecases/SaveAndSendAccountTest.java
+++ src/test/java/com/bank/transfers/application/app/usecases/SaveAndSendAccountTest.java
@@ -5,11 +5,14 @@ import com.bank.transfers.application.app.repositories.IAccountRepository;
 import com.bank.transfers.application.app.repositories.ITransferRepository;
 import com.bank.transfers.application.app.usecases.impl.SaveAndSendAccount;
 import com.bank.transfers.application.domains.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -20,6 +23,8 @@ import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -33,18 +38,18 @@ public class SaveAndSendAccountTest {
     private ITransferRepository bankTransferRepository;
     @Mock
     private ISendMessage sendBankTransferMessage;
-
-    @Before
-    public void init() {
-        final var objectMapper = JsonMapper.builder()
-                .addModule(new JavaTimeModule())
-                .build();
-        ReflectionTestUtils.setField(saveAndSendAccount, "objectMapper", objectMapper);
-    }
+    @Mock
+    private ObjectMapper objectMapper;
+    @Captor
+    private ArgumentCaptor<String> stringArgumentCaptor;
 
     @Test
     public void testSaveAndSend() {
 
+        final var objectMapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+        ReflectionTestUtils.setField(saveAndSendAccount, "objectMapper", objectMapper);
         final var user = User.of("1", "Leonardo Dantas", "12356547987", "user@mail.com");
 
         final var transfer = Transfer.of("456123", BigDecimal.valueOf(600));
@@ -55,5 +60,21 @@ public class SaveAndSendAccountTest {
         final var result = saveAndSendAccount.execute(transfer, account);
         assertThat(result).isNotNull();
         assertThat(result.value()).isEqualTo(BigDecimal.valueOf(600));
+
+        verify(sendBankTransferMessage).execute(anyString(), anyString());
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSaveAndSendThrowRuntimeException() throws JsonProcessingException {
+        final var user = User.of("1", "Leonardo Dantas", "12356547987", "user@mail.com");
+
+        final var transfer = Transfer.of("456123", BigDecimal.valueOf(600));
+        final var account = Account.from("1", "1", "456123", "8", LocalDateTime.of(2021, 10, 3, 10, 30, 0), LocalDateTime.of(2021, 10, 3, 10, 30, 0), LocalDateTime.of(2021, 10, 5, 10, 30, 0), LocalDateTime.of(2021, 10, 5, 10, 30, 0)).withDeposit(CashDeposit.of(user, BigDecimal.valueOf(100), TransferType.USER_DEPOSIT));
+
+        when(objectMapper.writeValueAsString(any()))
+                .thenThrow(JsonProcessingException.class);
+
+        saveAndSendAccount.execute(transfer, account);
     }
 }
